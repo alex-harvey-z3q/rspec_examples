bbce11a3909ae34b0fb3153c85aac6e5b6ddf501
Alex Harvey <alex@razorconsulting.com.au>
2016-04-23 19:22:30 +1000

    Add test for raise - 0.0.3

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -13,4 +13,9 @@ describe '#hm2s' do
   it 'should convert 30m to 1800' do
     expect(hm2s('30m')).to eq 1800
   end
+
+  it 'should raise an error given badly formatted input' do
+    expect { hm2s('I_am_badly_formatted') }.
+      to raise_error(RuntimeError, /illegal input/)
+  end
 end
